089f2f193a73ebe45b5f9b2ba827a7b7273d3df7
Ivy-fine <752874980@qq.com>
2019-09-03 17:09:04 +0800

    整合muen练习

--- src/router.js
+++ src/router.js
@@ -1,25 +1,52 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
-      component: Home
+      meta:{
+        required:true
+      },
+      component: () => import('./views/home/home.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/login',
+      name: 'login',
+      component: () => import('./views/login/login.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('./views/register/register.vue')
+    },
+    {
+      path:'*',
+      redirect:'/home'
     }
   ]
 })
+
+router.beforeEach((to,from,next)=>{
+  if(to.matched.some(item=>item.meta.required)){
+    let token=localStorage.token;
+    if(!token){
+      next({
+        path:'/login',
+        query:{
+          redirect:to.fullPath
+        }
+      })
+    }else{
+      next()
+    }
+  }else{
+    next()
+  }
+})
+export default router;
